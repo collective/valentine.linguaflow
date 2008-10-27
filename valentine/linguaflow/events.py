4321797fa7f94bf30645d62dd25de41945d22e05
svincic <svincic@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-10-27 21:00:27 +0000

    possibility to dump small phrases from xliff translations to .po files, no need to pay twice for the same translation you can now let xliffmarshall fire a ISyncWorkfloEvent if valentine.linguaflow is available. You create then a subscriber for this event which knows how to synchronize the workflow state for the translation. This will allow you to automatically set the same state on translations imported from a xliff file as the canonical since most of the time the translations are already checked or should be correct.

--- valentine/linguaflow/events.py
+++ valentine/linguaflow/events.py
@@ -24,10 +24,33 @@ class TranslationObjectUpdate(object):
         self.translation = translation
         self.action = action
         self.comment = comment
-    
+
 def notifyCanonicalUpdate(obj, event):
     # catch all ITranslatable modified
     if event.comment:
         wt = getToolByName(obj, 'portal_workflow')
         if 'linguaflow' in wt.getChainFor(obj):
             wt.doActionFor(event.translation, event.action, comment=event.comment )
+
+
+class ISyncWorkflowEvent(IObjectEvent):
+    """ Make sure translation workflow status is the same as canonicals. """
+
+    object = Attribute("The canonical object.")
+    translation = Attribute("The translation target object.")
+    comment = Attribute("The workflow comment, should contain which fields have changed.")
+
+
+class SyncWorkflowEvent(object):
+    """ """
+
+    implements(ISyncWorkflowEvent)
+
+    def __init__(self, context, translation, comment):
+        self.object = context
+        self.translation = translation
+        self.comment = comment
+
+
+
+    
