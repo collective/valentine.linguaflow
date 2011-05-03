e95a6291f59fef58e0183e9aaaf33c63a6aed9f7
alecghica <alecghica@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-05-03 16:31:20 +0000

    Work in progress: Plone 4 compatibility

--- valentine/linguaflow/setuphandlers.py
+++ valentine/linguaflow/setuphandlers.py
@@ -5,6 +5,8 @@ def setupVarious(context):
     if context.readDataFile('valentine.linguaflow_various.txt') is None:
         return
 
+    logger = context.getLogger('valentine.linguaflow')
+
     # Add linguaflow workflow as parallel default workflow
     portal = context.getSite()
     wf = getToolByName(portal, 'portal_workflow')
@@ -12,9 +14,13 @@ def setupVarious(context):
     if 'linguaflow' not in default_chain:
         default_chain.append('linguaflow')
         wf.setDefaultChain(' '.join(default_chain))
+        logger.info("Valentine Linguaflow: add linguaflow workflow as parallel default workflow")
 
     # Add linguaflow for folder workflow too if it doesn't use the default
     folder_chain = list(wf.getChainForPortalType('Folder'))
     if 'linguaflow' not in folder_chain:
         folder_chain.append('linguaflow')
         wf.setChainForPortalTypes(('Folder',), folder_chain)
+        logger.info("Valentine Linguaflow: add linguaflow for folder workflow")
+
+    logger.info("Valentine Linguaflow: done setting import steps")
