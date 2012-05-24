fe521581aa6bfbd8f317783a7f10496f721bd46d
avoinea <avoinea@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2012-05-24 13:53:34 +0000

    * Bug fix: Force unicode for Titles in getDeletableLanguages.py   [prospchr fixes #5165]

--- valentine/linguaflow/skins/linguaflow_templates/getDeletableLanguages.py
+++ valentine/linguaflow/skins/linguaflow_templates/getDeletableLanguages.py
@@ -5,8 +5,9 @@ lang_names = context.portal_languages.getAvailableLanguages()
 translations = context.getNonCanonicalTranslations()
 
 # Return tuples of lang id, lang name and content title
-languages = [(lang, lang_names[lang], translations[lang][0].Title(),  translations[lang][1])
-             for lang in translations.keys()]
+# Force unicode for Titles
+languages = [(lang, lang_names[lang], unicode(translations[lang][0].Title(),'utf-8'), translations[lang][1])
+            for lang in translations.keys()]
 
 def lcmp(x, y):
     return cmp(x[1], y[1])
